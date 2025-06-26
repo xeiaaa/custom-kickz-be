fe1e7dbae941863d5da9df8e140ebe486b48f45d
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2025-06-27 00:16:50 +0800

    temporary cors origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://custom-kickz-be.onrender.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
